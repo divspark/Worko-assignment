73e2d050a1f5da1789796982bff822729cc72205
divspark <divyansh2004mhj@gmail.com>
2024-06-25 12:26:36 +0530

    7th commit

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-import express from 'express'
+import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import connectDB from './DB/connect.js';
 import userRoutes from './routes/userRoutes.js';
-import referralRoutes from './routes/referralRoutes.js'
+import referralRoutes from './routes/referralRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(express.json());
 
-
+// Define __filename and __dirname to use in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Load environment variables
 dotenv.config();
 
+// Connect to MongoDB
 connectDB();
 
 // Set up EJS as the template engine
@@ -24,15 +25,65 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files (CSS, images, etc.) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Parse JSON bodies
+app.use(express.json());
 
-//Routes
-
+// Routes
 app.use('/user', userRoutes);
-app.use('/refferal', referralRoutes);
+app.use('/referral', referralRoutes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
 
 
-app.listen(process.env.port,()=>{
-    console.log(`Server is listening at ${port}`);
-})
+
+
+
+// import express from 'express'
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import dotenv from 'dotenv';
+// import connectDB from './DB/connect.js';
+// import userRoutes from './routes/userRoutes.js';
+// import referralRoutes from './routes/referralRoutes.js'
+
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Set up EJS as the template engine
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// // Middleware to parse form data
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.json());
+
+// dotenv.config();
+
+// connectDB();
+
+
+
+
+// //Routes
+
+// app.use('/user', userRoutes);
+// app.use('/refferal', referralRoutes);
+
+
+// app.listen(port, () => {
+//     console.log(`Server is listening at http://localhost:${port}`);
+// });
